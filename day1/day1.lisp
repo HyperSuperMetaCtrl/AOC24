a9e86435bc315bc75cf8449fa75ea5776e5059fb
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2024-12-01 14:39:30 +0100

    Day 1 Part 2

--- day1/day1.lisp
+++ day1/day1.lisp
@@ -1,13 +1,16 @@
 (defparameter *input-path* "input")
 
 (defun get-input ()
+  "read file into lines"
   (uiop:read-file-lines *input-path*))
 
 (defun split-line (line)
+  "split lines into two lists and parse from string to integer"
   (mapcar #'parse-integer
 	  (cl-utilities:split-sequence #\Space line :remove-empty-subseqs t)))
 
 (defun parse-input ()
+  "return the complete parsed input"
   (let ((lines (get-input)))
     (loop for line in lines
 	  for (x y) = (split-line line)
@@ -16,15 +19,26 @@
 	  finally (return (list xs ys)))))
 
 (defun sort-lists (lists)
+  "lists is a lists of lists and returns the lists sorted"
   (mapcar (lambda (lst) (sort lst #'<)) lists))
 
 (defun calculate-differences (sorted-lists)
+  "calculate the elementewise difference between two sorted lists"
   (let ((list1 (first sorted-lists))
 	(list2 (second sorted-lists)))
     (loop for x in list1
 	  for y in list2
 	  sum (abs (- x y)))))
 
+;; Day 1 Part 1
 (let* ((parsed-input (parse-input))
        (sorted-input (sort-lists parsed-input)))
   (format t "Sum of differences: ~A~%" (calculate-differences sorted-input)))
+
+;; Day 1 Part 2
+(let* ((parsed-input (parse-input))
+       (left-list (first parsed-input))
+       (right-list (second parsed-input))
+       (sum (loop for left in left-list
+		  sum (* left (count left right-list)))))
+  (format t "Sum of Products: ~A~%" sum))
