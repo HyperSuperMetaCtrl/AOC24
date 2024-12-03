c5783f2048d20eeb22ce38c5c9ea19b63e11b017
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2024-12-03 10:21:29 +0100

    Add Day 3 Part 2

--- day3/day3.lisp
+++ day3/day3.lisp
@@ -5,9 +5,21 @@
 (defun part1 (input)
   (let ((sum 0))
     (ppcre:do-register-groups ((#'parse-integer a b)) ("mul\\((\\d+),(\\d+)\\)" input)
-      (setq sum (+ sum (* a b))))
+      (setf sum (+ sum (* a b))))
+    sum))
+
+(defun part2 (input)
+  (let ((sum 0)
+	(state 'enabled))
+    (ppcre:do-register-groups (enable (#'parse-integer a b))
+	("(don't\\(\\)|do\\(\\))|mul\\((\\d+),(\\d+)\\)" input)
+      (when (and (eq state 'enabled) (string= enable "don't()")
+		 (setf state 'disabled)))
+      (when (and (eq state 'disabled) (string= enable "do()")
+		 (setf state 'enabled)))
+      (when (and (eq state 'enabled) a b)
+	         (setf sum (+ sum (* a b)))))
     sum))
 
 (format t "Part1: ~d~%" (part1 *input*))
-
-
+(format t "Part2: ~d~%" (part2 *input*))
